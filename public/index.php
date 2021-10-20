4c8d9864006a72f179fb217c452a7e1909951589
Antoine Leclabart <antoine.leclabart@gmail.com>
2021-10-20 12:29:49 +0200

    finsihed

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 // Get a $twig object from this file.
 require_once __DIR__ . '/../config/twig.php';
+require_once __DIR__ . '/../src/Products.php';
 
-
-$name = 'Wilder';
-
-echo $twig->render('home.html.twig', ['name' => $name]);
+echo $twig->render('products.html.twig', ['products' => $products]);
